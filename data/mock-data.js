9742f33d616d37f01afb948dcc53ed9cac246dd6
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2022-08-05 00:48:42 -0400

    first commit

---
+++ data/mock-data.js
@@ -0,0 +1,109 @@
+export const rec_channels = [
+  {
+    id: 1,
+    avatar: "https://robohash.org/veniamutarchitecto.png?size=50x50&set=set1",
+    username: "jkarlsen0",
+    game_name: "Cardguard",
+    rank: 36,
+  },
+  {
+    id: 2,
+    avatar:
+      "https://robohash.org/similiqueoccaecatiaut.png?size=50x50&set=set1",
+    username: "hlaurie1",
+    game_name: "Home Ing",
+    rank: 86,
+  },
+  {
+    id: 3,
+    avatar: "https://robohash.org/sitomnisdolorem.png?size=50x50&set=set1",
+    username: "eyokley2",
+    game_name: "Tin",
+    rank: 15,
+  },
+  {
+    id: 4,
+    avatar:
+      "https://robohash.org/earumsolutaconsequatur.png?size=50x50&set=set1",
+    username: "lfockes3",
+    game_name: "Trippledex",
+    rank: 35,
+  },
+  {
+    id: 5,
+    avatar:
+      "https://robohash.org/assumendaistereiciendis.png?size=50x50&set=set1",
+    username: "mdiego4",
+    game_name: "Subin",
+    rank: 98,
+  },
+  {
+    id: 6,
+    avatar: "https://robohash.org/nonsedrerum.png?size=50x50&set=set1",
+    username: "clagen5",
+    game_name: "Bamity",
+    rank: 15,
+  },
+  {
+    id: 7,
+    avatar: "https://robohash.org/suscipitcumquererum.png?size=50x50&set=set1",
+    username: "wmasters6",
+    game_name: "Toughjoyfax",
+    rank: 65,
+  },
+  {
+    id: 8,
+    avatar: "https://robohash.org/autetunde.png?size=50x50&set=set1",
+    username: "ecliff7",
+    game_name: "Zontrax",
+    rank: 70,
+  },
+  {
+    id: 9,
+    avatar: "https://robohash.org/voluptasquiex.png?size=50x50&set=set1",
+    username: "acharpling8",
+    game_name: "Toughjoyfax",
+    rank: 95,
+  },
+  {
+    id: 10,
+    avatar: "https://robohash.org/laborequodad.png?size=50x50&set=set1",
+    username: "smeach9",
+    game_name: "Keylex",
+    rank: 76,
+  },
+  ,
+];
+
+export const top_channels = [
+  {
+    id: 1,
+    avatar: "https://robohash.org/veniamutarchitecto.png?size=50x50&set=set1",
+    username: "jkarlsen0",
+    game_name: "Cardguard",
+    rank: 36,
+  },
+  {
+    id: 2,
+    avatar:
+      "https://robohash.org/similiqueoccaecatiaut.png?size=50x50&set=set1",
+    username: "hlaurie1",
+    game_name: "Home Ing",
+    rank: 86,
+  },
+  {
+    id: 3,
+    avatar: "https://robohash.org/sitomnisdolorem.png?size=50x50&set=set1",
+    username: "eyokley2",
+    game_name: "Tin",
+    rank: 15,
+  },
+  {
+    id: 4,
+    avatar:
+      "https://robohash.org/earumsolutaconsequatur.png?size=50x50&set=set1",
+    username: "lfockes3",
+    game_name: "Trippledex",
+    rank: 35,
+  },
+];
